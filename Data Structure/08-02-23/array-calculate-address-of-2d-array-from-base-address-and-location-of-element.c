0cd378d2a40475e9a71c7e3c52ebef51a2a5c603
Yugmi-7791 <124332095+Yugmi-7791@users.noreply.github.com>
2023-02-20 15:58:24 +0530

    Initial commit

--- Data Structure/08-02-23/array-calculate-address-of-2d-array-from-base-address-and-location-of-element.c
+++ Data Structure/08-02-23/array-calculate-address-of-2d-array-from-base-address-and-location-of-element.c
@@ -1,22 +1,24 @@
+//Calculate address of 2d array from base address and location of element
+
 #include <stdio.h>
 
 int
 main ()
 {
   int ba, c, i, j, m, n, address, address1;
-  printf ("Enter base address:");
+  printf ("Enter base address:");//entering base address
   scanf ("%d", &ba);
-  printf ("Enter index of the element: ");
+  printf ("Enter index of the element: ");//entering index of element
   scanf ("%d %d", &i, &j);
-  printf ("Enter of one element : ");
+  printf ("Enter of one element : ");//entering of one element
   scanf ("%d", &c);
-  printf ("Enter total number of columns : ");
+  printf ("Enter total number of columns : ");//entering total number of columns
   scanf ("%d", &m);
-  printf ("Enter total number of rows : ");
+  printf ("Enter total number of rows : ");//entering total number of rows
   scanf ("%d", &n);
 
   address = ba + ((i - 1) * m + (j - 1)) * c;
-
+//printing address of 2d array
   printf ("%d", address);
 
 
