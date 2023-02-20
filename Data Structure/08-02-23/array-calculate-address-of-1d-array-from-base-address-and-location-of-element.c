360855647d72c8a69458b1da2e2cc0fcbc7dec29
Yugmi-7791 <124332095+Yugmi-7791@users.noreply.github.com>
2023-02-20 15:55:06 +0530

    Initial commit

--- Data Structure/08-02-23/array-calculate-address-of-1d-array-from-base-address-and-location-of-element.c
+++ Data Structure/08-02-23/array-calculate-address-of-1d-array-from-base-address-and-location-of-element.c
@@ -1,3 +1,5 @@
+//Program to calculate address of 1d array from base address and location of element
+
 #include <stdio.h>
 
 int
@@ -10,9 +12,10 @@ main ()
   scanf ("%d", &i);
   printf ("Enter of one element : ");
   scanf ("%d", &c);
-
+  
+//calculating address of 1d array
   address = ba + (i - 1) * c;
-  printf ("%d", address);
+  printf ("%d", address);//printing elememts
 
   return 0;
 }
