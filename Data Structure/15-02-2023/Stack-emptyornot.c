b3ff4d82dddea3f98920847242dafd9cf7ed5aa1
Yugmi-7791 <124332095+Yugmi-7791@users.noreply.github.com>
2023-02-21 11:08:10 +0530

    Initial commit

--- Data Structure/15-02-2023/Stack-emptyornot.c
+++ Data Structure/15-02-2023/Stack-emptyornot.c
@@ -1,5 +1,8 @@
+//Program to find if a stack is empty or not
+
 #include <stdio.h>
 #include <stdlib.h>
+//implimentation of stack
 struct stack
 {
     int size;
@@ -7,6 +10,7 @@ struct stack
     int *arr;
 };
 
+//function to check stack is empty or not
 int isempty(struct stack *sp)
 {
     if(sp->top==-1)
@@ -20,7 +24,7 @@ int main()
     struct stack *sp=(struct stack*)malloc(sizeof(struct stack));
     sp->size=5;
     sp->top=-1;
-    sp->arr=(int*)malloc(sp->size*sizeof(int));
-    isempty(sp);
+    sp->arr=(int*)malloc(sp->size*sizeof(int));//assigning size in heap
+    isempty(sp);//function calling
     return 0;
 }
